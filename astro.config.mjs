b6134a77a58dbef4201267d3d7c8854cf8ec6113
Roy Mosby <ego.madking@gmail.com>
2023-06-04 17:14:41 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify/functions';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://nostories.recipes',
+  integrations: [react(), sitemap()],
   output: 'static',
 });
